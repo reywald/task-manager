1875abcb963b602319e0b155f8a6f5a46425ea9d
ikechukwu.agbarakwe <iaweepingsaint@gmail.com>
2024-07-14 17:40:31 +0100

    Loops and Stores

--- src/ToDoInputForm.svelte
+++ src/ToDoInputForm.svelte
@@ -1,5 +1,15 @@
 <script>
-    export let userName
+  import { toDoItems } from "./store.js";
+  export let userName;
+  let newItem = "";
+
+  function addToList() {
+    $toDoItems = [...$toDoItems, { text: newItem, status: false }];
+    newItem = "";
+  }
 </script>
+
+<input bind:value={newItem} type="text" placeholder="Add a new task" />
+<button on:click={addToList}>Add</button>
 
 <h3>{userName}'s task list:</h3>
--- src/ToDoList.svelte
+++ src/ToDoList.svelte
@@ -1,1 +1,26 @@
-<h2>ToDoList Component</h2>
+<script>
+  import { toDoItems } from "./store.js";
+
+  function removeFromList(i) {
+    $toDoItems.splice(i, 1);
+    $toDoItems = $toDoItems;
+  }
+</script>
+
+{#each $toDoItems as item, index}
+  <div class="toDoItems">
+    <input bind:checked={item.status} type="checkbox" />
+    <span class:checked={item.status}>{item.text}</span>
+    <button on:click={() => removeFromList(index)}>Delete</button>
+    <br />
+  </div>
+{/each}
+
+<style>
+  .checked {
+    text-decoration: line-through;
+  }
+  .toDoItems {
+    text-align: left;
+  }
+</style>
